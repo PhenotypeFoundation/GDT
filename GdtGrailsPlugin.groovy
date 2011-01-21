a7774772271c81763103ef221cfdafb13c4e3834
4np <work@osx.eu>
2011-01-21 12:13:10 +0000

    - set plugin info

--- GdtGrailsPlugin.groovy
+++ GdtGrailsPlugin.groovy
@@ -1,24 +1,40 @@
+/**
+ *  GDT, a plugin for Grails Domain Templates
+ *  Copyright (C) 2011 Jeroen Wesbeek
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  $Author$
+ *  $Rev$
+ *  $Date$
+ */
+
 class GdtGrailsPlugin {
-    // the plugin version
-    def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.6 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
+    def version			= "0.1"
+    def grailsVersion	= "1.3.4 > *"
+    def dependsOn		= [ajaxflow: "0.1.18 > *"]
+    def pluginExcludes	= [
             "grails-app/views/error.gsp"
     ]
-
-    // TODO Fill in these fields
-    def author = "Your name"
-    def authorEmail = ""
-    def title = "Plugin summary/headline"
-    def description = '''\\
-Brief description of the plugin.
+    def author			= "Jeroen Wesbeek"
+    def authorEmail 	= "work@osx.eu"
+    def title			= "Grails Domain Templates"
+    def description 	= '''\\
+Grails Domain Templates allows you to dynamically extend domain classes with unlimited user definable fields based on a Template for a specific Domain Class.
+If you, for example, have a Company Domain Class, but companies would like to register different fields for such an entity, several Templates could be created
+for every type of company (e.g. a financial institution template, university template, etc). Using these Templateted Domain Classed creates more dynamic
+flexibility in the application.
 '''
-
-    // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/gdt"
 
     def doWithWebDescriptor = { xml ->
