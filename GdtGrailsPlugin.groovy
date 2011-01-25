b91d170ecb9506b12412806469ee62a662800c1c
4np <work@osx.eu>
2011-01-25 10:20:11 +0000

    - to 0.0.2

--- GdtGrailsPlugin.groovy
+++ GdtGrailsPlugin.groovy
@@ -20,7 +20,7 @@
  */
 
 class GdtGrailsPlugin {
-    def version			= "0.0.1"
+    def version			= "0.0.2"
     def grailsVersion	= "1.3.4 > *"
 //    def dependsOn		= [ajaxflow: "0.1.18 > *"]
     def dependsOn		= [:]
